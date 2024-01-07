18cd3d8e353ece652aa19f5810ea7b5159202e41
aydosjs <aji3385@gmail.com>
2024-01-07 11:53:15 +0500

    some bug fix

--- app/components/Different.tsx
+++ app/components/Different.tsx
@@ -3,7 +3,7 @@ const Different = () => {
     <div className="flex flex-col space-y-10 text-left w-full">
 
       <div className="flex flex-col-reverse md:flex-row space-x-4 mb-32">
-        <div className="w-ful md:w-2/3 ">
+        <div className="w-ful md:w-2/3">
           <h1 className="text-left text-4xl md:text-5xl mb-6 font-bold">
             <span className="text-8xl">H</span>ow is this roadmap different?
           </h1>
@@ -14,7 +14,7 @@ const Different = () => {
             understand how to be a modern day programmer.
           </p>
         </div>
-        <div className="w-ful mb-20 md:mb-0 md:w-1/3 flex items-center justify-center relative">
+        <div className="w-ful mb-20 md:mb-0 md:w-1/3 flex items-center justify-center relative min-h-52">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-32 h-32 animate-bounce">
             <path fillRule="evenodd" d="M8.157 2.176a1.5 1.5 0 0 0-1.147 0l-4.084 1.69A1.5 1.5 0 0 0 2 5.25v10.877a1.5 1.5 0 0 0 2.074 1.386l3.51-1.452 4.26 1.762a1.5 1.5 0 0 0 1.146 0l4.083-1.69A1.5 1.5 0 0 0 18 14.75V3.872a1.5 1.5 0 0 0-2.073-1.386l-3.51 1.452-4.26-1.762ZM7.58 5a.75.75 0 0 1 .75.75v6.5a.75.75 0 0 1-1.5 0v-6.5A.75.75 0 0 1 7.58 5Zm5.59 2.75a.75.75 0 0 0-1.5 0v6.5a.75.75 0 0 0 1.5 0v-6.5Z" clipRule="evenodd" />
           </svg>
@@ -25,7 +25,7 @@ const Different = () => {
         Projects Built
       </h1>
 
-      <div className="flex flex-col md:flex-row space-y-4 md:space-x-4">
+      <div className="flex flex-col md:flex-row space-y-4 sm:space-y-0 md:space-x-4">
 
         <div className="w-full md:w-1/2 border border-neutral-800 rounded-md p-6 group cursor-pointer bg-white bg-opacity-5 hover:backdrop-blur-[1px] backdrop-filter">
           <img
--- app/components/HeroSectionWithEmailSignup.tsx
+++ app/components/HeroSectionWithEmailSignup.tsx
@@ -25,7 +25,7 @@ const HeroSectionWithEmailSignup = () => {
   }, []);
 
   return (
-    <div className="pb-32 md:pb-16 flex items-center flex-col space-y-10 min-h-[calc(100vh-20px)] md:min-h-[calc(100vh-85px)] justify-center">
+    <div className="pb-32 md:pb-16 flex items-center flex-col space-y-10 min-h-[calc(100dvh-20px)] md:min-h-[calc(100dvh-85px)] justify-center">
 
       <Spotlight
         className="-top-0 sm:-top-32 left-10 md:-top-24 md:left-10 lg:left-60 lg:-top-10 2xl:-top-32 2xl:left-80"
@@ -33,9 +33,9 @@ const HeroSectionWithEmailSignup = () => {
       />
 
       <div>
-        <TextGenerateEffect delay={0.4} words={'Learn. Build. Grow.'} className="text-6xl md:text-7xl font-bold text-center mb-2" />
+        <TextGenerateEffect delay={0.4} words={'Learn. Build. Grow.'} className="text-4xl md:text-7xl font-bold text-center mb-2" />
         <motion.p
-          className="transition-all duration-300 ease-in-out delay-150 text-lg md:text-xl text-center font-normal text-gray-400"
+          className="transition-all duration-300 ease-in-out delay-150 text-xs md:text-xl text-center font-normal text-gray-400"
           initial={{ opacity: 0 }}
           animate={{ opacity: isMounted ? 1 : 0 }}
           transition={{ duration: 3 }}
@@ -54,7 +54,7 @@ const HeroSectionWithEmailSignup = () => {
         <input
           placeholder="Your email"
           type="text"
-          className="p-5 border pr-14 border-white border-opacity-20 bg-white bg-opacity-10 group-hover:backdrop-blur-[1px] backdrop-filter rounded-full outline-none w-96 "
+          className="p-5 border pr-14 border-white border-opacity-20 bg-white bg-opacity-10 group-hover:backdrop-blur-[1px] backdrop-filter rounded-full outline-none w-64 sm:w-96 "
         />
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="absolute w-8 h-8 top-1/2 transition-all -translate-y-1/2 right-4 bg-opacity-20 cursor-pointer bg-white opacity-50 group-hover:opacity-100 text-white p-1 rounded-full">
           <path fillRule="evenodd" d="M3 10a.75.75 0 0 1 .75-.75h10.638L10.23 5.29a.75.75 0 1 1 1.04-1.08l5.5 5.25a.75.75 0 0 1 0 1.08l-5.5 5.25a.75.75 0 1 1-1.04-1.08l4.158-3.96H3.75A.75.75 0 0 1 3 10Z" clipRule="evenodd" />
