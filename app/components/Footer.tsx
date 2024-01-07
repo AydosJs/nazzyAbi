e586abe3b6f077405c230b1fbd4b3c437003577b
aydosjs <aji3385@gmail.com>
2024-01-07 19:50:12 +0500

    footer positions fixxed

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,15 +3,15 @@ import Link from "next/link"
 const Footer = () => {
   return (
     <footer className="text-white border-t border-neutral-800 mt-20 backdrop-blur-sm backdrop-filter body-font">
-      <div className="container p-8 mx-auto flex items-center sm:justify-between flex-col sm:flex-row space-y-6 sm:space-y-0">
-        <div className="flex flex-row items-center space-x-2">
+      <div className="container p-8 mx-auto flex items-center sm:justify-between flex-col-reverse sm:flex-row gap-4 sm:gap-0 sm:space-y-0">
+        <div className="flex flex-col-reverse sm:flex-row items-center sm:space-x-2">
           <p className="text-sm text-neutral-500 font-medium">
             © 2024 RoadMap
           </p>
           <span className="hidden sm:inline-block text-neutral-700">
             |
           </span>
-          <Link target="blank" href={'https://www.youtube.com/@NizzyABI'} className="hover:underline transition-all text-sm flex flex-row items-center">
+          <Link target="blank" href={'https://www.youtube.com/@NizzyABI'} className="mb-4 sm:mb-0 hover:underline transition-all text-sm flex flex-row items-center">
             <svg className='w-6 h-6 mr-2' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
               <path fill="#FF3D00" d="M43.2,33.9c-0.4,2.1-2.1,3.7-4.2,4c-3.3,0.5-8.8,1.1-15,1.1c-6.1,0-11.6-0.6-15-1.1c-2.1-0.3-3.8-1.9-4.2-4C4.4,31.6,4,28.2,4,24c0-4.2,0.4-7.6,0.8-9.9c0.4-2.1,2.1-3.7,4.2-4C12.3,9.6,17.8,9,24,9c6.2,0,11.6,0.6,15,1.1c2.1,0.3,3.8,1.9,4.2,4c0.4,2.3,0.9,5.7,0.9,9.9C44,28.2,43.6,31.6,43.2,33.9z"></path><path fill="#FFF" d="M20 31L20 17 32 24z"></path>
             </svg>
